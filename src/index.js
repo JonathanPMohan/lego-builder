1a7b3e69d79ab56681e64d109a15d2f828c8dfe5
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-06 19:33:42 -0600

    Updated Everything

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
+import './index.scss';
+import legoPhoto from './images/lego.png';
+
+$('#legoMan').attr('src', legoPhoto);
+
+console.log('we here now');
