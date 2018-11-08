7bd89759b726bd471e1b7355edc37804888af56a
Jonathan Mohan <JonathanMohan.net@gmail.com>
2018-11-07 19:54:48 -0600

    Got Legos Printing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 import legoPhoto from './images/lego.png';
+import legoCharacter from './javascripts/components/legoCharacter/legoCharacter';
 
 $('#legoMan').attr('src', legoPhoto);
 
-console.log('we here now');
+const initApp = () => {
+  // events.setEvents();
+  legoCharacter.initView();
+  legoCharacter.getParts();
+};
+
+initApp();
